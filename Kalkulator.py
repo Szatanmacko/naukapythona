b4d5b7871dcc0dd6db6aa6b5d8b0d0ce1bb5aedb
Maciej Straczynski <straczynski.maciej@gmail.com>
2021-11-04 23:16:23 +0100

    Changes by plasota

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,6 +1,7 @@
-import sys
+# Shift + F6 - zmień nazwę zmiennej i wszystkie użycia
+# Ctrl + Q na funkcji / klasie / zmiennej - dokumentacja do niej (jeśli jest)
+# Ctrl + Alt + L - przeformatuj cały otwarty plik (jak będziesz miał coś zaznaczone to tylko formatuje zaznaczone)
 print("Kalkulator")
-print("Podaj pierwsza liczbe")
 
 a = float(input())
 
